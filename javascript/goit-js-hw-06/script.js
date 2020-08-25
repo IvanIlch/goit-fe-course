3f5f02a83a7cbc4e039c673ab55a2786004d0919
Ivan <ilchenkoivan92@gmail.com>
2020-08-25 23:52:25 +0300

    first commit

---
+++ javascript/goit-js-hw-06/script.js
@@ -0,0 +1,69 @@
+import users from "/users.js";
+// Task-1
+
+const getUserNames = (users) => users.map((user) => user.name);
+console.log(getUserNames(users));
+
+// Task-2
+
+const getUsersWithEyeColor = (users, color) =>
+  users.filter((user) => user.eyeColor === color).map((user) => user.name);
+console.log(getUsersWithEyeColor(users, "blue"));
+// Task-3
+
+const getUsersWithGender = (users, gender) =>
+  users.filter((user) => user.gender === gender).map((user) => user.name);
+console.log(getUsersWithGender(users, "male"));
+
+// Task-4
+const getInactiveUsers = (users) =>
+  users.filter((user) => !user.isActive).map((user) => user.name);
+
+console.log(getInactiveUsers(users));
+// Task-5
+const getUserWithEmail = (users, email) =>
+  users.find((user) => user.email === email);
+
+console.log(getUserWithEmail(users, "shereeanthony@kog.com"));
+console.log(getUserWithEmail(users, "elmahead@omatom.com"));
+// Task-6
+const getUsersWithAge = (users, min, max) =>
+  users.filter((user) => user.age > min && user.age < max);
+
+console.log(getUsersWithAge(users, 20, 30));
+
+console.log(getUsersWithAge(users, 30, 40));
+
+// Task-7
+const calculateTotalBalance = (users) =>
+  users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+
+console.log(calculateTotalBalance(users));
+// Task-8
+
+const getUsersWithFriend = (users, friendName) =>
+  users
+    .filter((user) => user.friends.includes(friendName))
+    .map((user) => user.name);
+
+console.log(getUsersWithFriend(users, "Briana Decker"));
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+// Task-9
+const getNamesSortedByFriendsCount = (users) =>
+  users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
+
+// Task-10
+const getSortedUniqueSkills = (users) =>
+  users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
+      return allSkills;
+    }, [])
+    .filter((value, index, arr) => arr.indexOf(value) === index)
+    .sort();
+
+console.log(getSortedUniqueSkills(users));
